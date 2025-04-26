4176b1b6f38207a513d4b28f28b0dd181b515db3
Naveen Eggadi <59551835+Naveen710@users.noreply.github.com>
2025-04-26 20:26:08 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,12 @@
+
 #!/bin/bash
 set -e
+# Stop and remove old container if running
+docker ps -q --filter "name=my-flask-app" | grep -q . && docker stop my-flask-app && docker rm my-flask-app
 
-# Pull the Docker image from Docker Hub
-docker pull naveen710/simple-python-flask-app
+# Pull latest image (if needed)
+docker pull your-repo/your-image:latest
 
-# Run the Docker image as a container
-docker run -d -p 5000: 5000 naveen710/simple-python-flask-app
+# Run new container
+docker run -d --name my-flask-app -p 5000:5000 naveen710/simple-python-flask-app:latest
+
